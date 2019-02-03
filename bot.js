00e31dbcd6d21ea703ade5f0e5240f0f5a89fadb
Hyacinth12303 <47294038+Hyacinth12303@users.noreply.github.com>
2019-02-03 18:50:15 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I feel alive');
+
+    //client.channels.find(x => x.name === 'ostentatious-declaration').send('ɢʀᴇᴇᴛɪɴɢs, ᴘᴀᴛʜᴇᴛɪᴄ ғᴀʟʟɪʙʟᴇ ᴄʀᴇᴀᴛᴜʀᴇs... ʜᴏᴡ ᴍᴀʏ ɪ ʜᴇʟᴘ ʏᴏᴜ?')
+});
+
+client.on('message', (msg) => {
+    if (msg.content === 'Hello Enurii') {
+        msg.channel.send('Greetings... ${msg.author.username}');
+    }
+});
+client.on('message', async msg => {
+    if (msg.content === '@everyone') {
+        msg.channel.send(`@everyone`)
+    }
+    if (msg.content === 'help') {
+        msg.channel.send(`@everyone`)
+    }
+    if (msg.content === 'stop') {
+        msg.channel.send(`@everyone`)
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
