8cd863e352353950b5a832fffb3ede3c40d92b5b
Hyacinth12303 <47294038+Hyacinth12303@users.noreply.github.com>
2019-02-03 20:54:23 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I feel alive');
 
-    client.channels.find(x => x.name === 'ostentatious-declaration').send('ɢʀᴇᴇᴛɪɴɢs, ᴘᴀᴛʜᴇᴛɪᴄ ғᴀʟʟɪʙʟᴇ ᴄʀᴇᴀᴛᴜʀᴇs... ʜᴏᴡ ᴍᴀʏ ɪ ʜᴇʟᴘ ʏᴏᴜ?')
+    //client.channels.find(x => x.name === 'ostentatious-declaration').send('ɢʀᴇᴇᴛɪɴɢs, ᴘᴀᴛʜᴇᴛɪᴄ ғᴀʟʟɪʙʟᴇ ᴄʀᴇᴀᴛᴜʀᴇs... ʜᴏᴡ ᴍᴀʏ ɪ ʜᴇʟᴘ ʏᴏᴜ?')
 });
 
 client.on('message', (msg) => {
-    if (msg.content === 'Hello Enurii') {
-        msg.channel.send('Greetings... <@%ID%> ');
+    if (msg.content === 'Hello Enurii') { 
+        msg.channel.send('Greetings... +y.mention()+');    
     }
 });
+
 client.on('message', async msg => {
     if (msg.content === '@everyone') {
         msg.channel.send(`@everyone`)
