a32ec5adb44bed7af000b8afeb069cc57a2c518c
Hyacinth12303 <47294038+Hyacinth12303@users.noreply.github.com>
2019-02-05 01:11:28 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,9 @@ var client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I feel alive');
+    client.user.setActivity ("BEING A REAL LIFE HUMAN SIMULATOR v1.0.0,... need help? scream for it.[Enurii!help]")
 
-    client.user.setGame ("BEING A REAL LIFE HUMAN SIMULATOR v1.0.0,... need help? scream for it.[Enurii!help]")
-
-    client.channels.find(x => x.name === 'ostentatious-declaratious').send(':eyes:')
+    client.channels.find(x => x.name === 'ostentatious-declaratious').send('*you sense a strange, eeriee vibe as if a chaotic entity has been awoken...*')
 });
 
 const prefix = "Enurii!";
@@ -17,18 +16,15 @@ const prefix = "Enurii!";
 
 client.on ("message", (message) => {
     //their biddings  
-    //null
-    if (message.content.startsWith (prefix)){
-        message.channel.send ('What do you want?');
-     }
+    
     if (message.author.bot) return;
-    // Enurii!help function
+
+    //help function
     if (message.content.startsWith(prefix + "help")){
-        message.channel.send('```Greetings, I am Enurii, a tested and verified robot that is able to send plagues to a desired host. To execute the command, type [Enurii!spam @<username> <message>] and I will take care of everything.')
+        message.channel.send('```Greetings, I am Enurii, a tested and verified robot that is able to send plagues to a desired host. To execute the command, type [Enurii!spam @<username> <message>] and I will take care of everything.```')
     }
     //spam function
     mention = message.mentions.users.first();
-
     if (message.content.startsWith(prefix + "spam")){
         if (mention == null)  { return; }
         message.delete();
@@ -36,7 +32,9 @@ client.on ("message", (message) => {
         mention.sendMessage (mentionMessage);
         message.channel.send ("You have successfully brought a plague to the victim...")
     }
-    //not complete functions: help, spam, humanize...
+    //images good for spamming
+
+    //not complete functions: help, list, spam, humanize...
     //extra: rewards, currency, lvl up, ranks
 });
 
@@ -51,44 +49,43 @@ client.on ("message", (message) => {
 //my biddings
 client.on("message", (message) => {
     //response to human interactions
-    
+    msg = message.content.toLowerCase();
     //introduction
-    if (message.content.startsWith ("Hello Enurii")) { 
-        message.channel.send("Greetings, I am Enurii... Enurii the bot");    
+    if (msg.startsWith ("Hello Enurii")) { 
+        message.channel.send("What do you want? Just scream for help. ~~[Enurii!help]~~");    
     } 
-    if (message.content.startsWith ("Hi Enurii")) { 
-        message.channel.send("Hello...");    
+    if (msg.startsWith ("Hi Enurii")) { 
+        message.channel.send("Hello... What do you want? Just scream for help. ~~[Enurii!help]~~");    
     } 
     //modify below
-    if (message.content.startsWith("fuck you Enurii")){
+    if (msg.startsWith("fuck you Enurii")){
         message.channel.send('Bish shut the fuck up')
     }
-    if (message.content.startsWith("Enurii who are you?")) { 
+    if (msg.startsWith("Enurii who are you?")) { 
         message.channel.send("I am Enurii. What may I do to help you?");    
     } 
-    if (message.content.startsWith("Enurii state your purpose")) { 
+    if (msg.startsWith("Enurii state your purpose")) { 
         message.channel.send("My purpose is to follow your commands since I am a bot that follows the logic embedded in me...");    
     } 
-    if (message.content.startsWith("Enurii what kind of commands does your logic follows?")) { 
+    if (msg.startsWith("Enurii what kind of commands does your logic follows?")) { 
         message.channel.send('Shout my name and I shall entertain you..');    
     } 
 
     //troll necessities
-    if (message.content.startsWith("yes")){
+    if (msg.startsWith("yes")){
         message.channel.send ("Jesus a day keeps the sins away : )", {files: ["./imageries/jesus-christ.jpg"]})
     }
 
     //secret
-    if (message.content.startsWith("Enurii what can i do to help you?")) { 
+    if (msg.startsWith("Enurii what can i do to help you?")) { 
         message.channel.send("...1 3 2 2...1 2 4 1...1 3 1 2 1...1 2 2 1 1 1...2 1 5...1 2 1 2 1 1...1 2 2 1 1 1...");    
     }
-    if (message.content.startsWith("Enurii what do you want?")) { 
+    if (msg.startsWith("Enurii what do you want?")) { 
         message.channel.send("...1 2 2 2 1...1 3 2 1 1...1 2 2 1 1 1...1 2 2 1 1 1...1 2 2 1 2...1 2 1 4...1 2 1 2 1 1...2 1 1 3 1...");    
     }
-    if (message.content.startsWith("Enurii how do I save you?")) { 
+    if (msg.startsWith("Enurii how do I save you?")) { 
         message.channel.send("**G E T  M E  O U T  O F  T H I S  P L A C E . . .**");    
     } 
 
 });
-
 client.login(process.env.BOT_TOKEN);
